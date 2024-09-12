492173cca2c2a32883d2b5dc29b8dc5792d5e008
Valvixx <valvixx04@gmail.com>
2024-09-12 22:49:22 +0300

    Adding Task4 and Task5 for lab1

--- AlgorithmsLabs/Lab1.cs
+++ AlgorithmsLabs/Lab1.cs
@@ -11,7 +11,7 @@ public class Lab1
 
         foreach (var l in array)
         {
-            if (array[l]>max)
+            if (array[l] > max)
             {
                 max = array[l];
             }
@@ -19,14 +19,14 @@ public class Lab1
 
         foreach (var l in array)
         {
-            if (array[l]<min)
+            if (array[l] < min)
             {
                 min = array[l];
             }
         }
 
         int result = max - min;
-        Console.WriteLine(result); 
+        Console.WriteLine(result);
     }
 
     public static void Task2()
@@ -43,7 +43,92 @@ public class Lab1
     {
         int size = Convert.ToInt32(Console.ReadLine());
         int[] array = new int[size];
-        
+
         Console.WriteLine($"Array size: {array.Length}");
+    }
+
+    public static void Task4()
+    {
+        int[,] array =
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            int rowSum = 0;
+            for (int j = 0; j < cols; j++)
+            {
+                rowSum += array[i, j];
+            }
+
+            Console.WriteLine($"Сумма строки {i + 1}: {rowSum}");
+        }
+    }
+
+    public static void Task5()
+    {
+        Student[] students = new Student[]
+        {
+            new Student("Иван", "Десятов", "ФВТ", 121231233),
+            new Student("Никита", "Петров", "ФВТ", 12341412),
+            new Student("Сергей", "Сергеев", "ФИТЭ", 331135132),
+            new Student("Мария", "Иванова", "ФВТ", 454234232)
+        };
+
+        Console.WriteLine("Введите имя для поиска:");
+        string searchFirstName = Console.ReadLine();
+        Console.WriteLine("Введите фамилию для поиска:");
+        string searchLastName = Console.ReadLine();
+
+        Student? foundStudent = FindStudent(students, searchFirstName, searchLastName);
+
+        if (foundStudent != null)
+        {
+            Console.WriteLine("Студент найден:");
+            foundStudent.Value.Display();
+        }
+        else
+        {
+            Console.WriteLine("Студент не найден.");
+        }
+    }
+
+    static Student? FindStudent(Student[] students, string firstName, string lastName)
+    {
+        foreach (var student in students)
+        {
+            if (student.FirstName == firstName && student.LastName == lastName)
+            {
+                return student;
+            }
+        }
+
+        return null;
+    }
+}
+
+struct Student
+{
+    public string FirstName, LastName, Faculty;
+    public int RecordBookNumber;
+
+    public Student(string firstName, string lastName, string faculty, int recordBookNumber)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Faculty = faculty;
+        this.RecordBookNumber = recordBookNumber;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Имя: {FirstName}, Фамилия: {LastName}, Факультет: {Faculty}," +
+                          $" Номер зачётки: {RecordBookNumber}");
     }
 }
--- AlgorithmsLabs/Program.cs
+++ AlgorithmsLabs/Program.cs
@@ -3,3 +3,5 @@
 Lab1.Task1();
 Lab1.Task2();
 Lab1.Task3();
+Lab1.Task4();
+Lab1.Task5();
